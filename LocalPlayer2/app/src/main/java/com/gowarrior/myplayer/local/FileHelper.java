b1264a240a4ea19cc4a55030813e40ce52d912bd
GoWarrior <support@gowarriorosh.com>
2015-06-04 13:45:00 +0800

    fix some error

--- LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/FileHelper.java
+++ LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/FileHelper.java
@@ -17,18 +17,16 @@ public class FileHelper {
 
     public final static String LOGTAG = "FileHelper";
 
-    //2.3  5没有使用去掉
-    //private  static File mRootFile;
-    //2.4
+
     private ExecutorService mExecutorService = null;
-    //1.构造
+
     private static FileHelper mFileHelper = null;
     private Context mContext = null;
 
-    //3 使用ReentrantLock的锁定机制
+
     private static ReentrantLock mLock = new ReentrantLock();
 
-    //2.文件与路径加载
+
     private String[] mImageSuffixes;
     private String[] mAudioSuffixes;
     private String[] mVideoSuffixes;
@@ -37,9 +35,10 @@ public class FileHelper {
     private String mFriendlyNameHd;
 
 
-    // 4.当前路径
+
     private String mCurrentPath;
     private boolean mNeedRefresh = false;
+
     //文件类型
     public enum FILETYPE {
         IMAGE, AUDIO, VIDEO, APK, DIR, UNKNOW;
@@ -50,7 +49,7 @@ public class FileHelper {
         NAME,TIME;
     }
 
-// Getter,Setter
+
     public String getCurrentPath() {
         return mCurrentPath;
     }
@@ -68,14 +67,9 @@ public class FileHelper {
         void onDirLoaded(String Path);
     }
 
-    //7. 去掉mOnDirLoadedListener 没有使用到
-//    private OnDirLoadedListener mOnDirLoadedListener = null;
-//
-//    public void setOnDirLoadedListener(OnDirLoadedListener listener) {
-//        mOnDirLoadedListener = listener;
-//    }
 
-    //1.构造函数 单例
+
+
     public static FileHelper getInstance(Context context) {
         if (mFileHelper == null && context != null) {
             mFileHelper = new FileHelper(context);
@@ -87,15 +81,15 @@ public class FileHelper {
             if (mContext == null && context != null) {
                 mContext = context;
             }
-        // 2.路径初始化
+
         initStoragePath();
     }
 
 
-    //2.路径相关
+
 
     private  void initStoragePath() {
-        //2.1 添加values/arrays.xml 指定文件类型
+
         mImageSuffixes = mContext.getResources().getStringArray(
                 R.array.fileEndingImage);
         mAudioSuffixes = mContext.getResources().getStringArray(
@@ -103,22 +97,18 @@ public class FileHelper {
         mVideoSuffixes = mContext.getResources().getStringArray(
                 R.array.fileEndingVideo);
 
-        //2.2 values/strings.xml 添加存储介质名称
+
         mFriendlyNameUsb = mContext.getResources().getString(R.string.friendly_name_usb);
         mFriendlyNameSd  = mContext.getResources().getString(R.string.friendly_name_sd);
         mFriendlyNameHd  = mContext.getResources().getString(R.string.friendly_name_hd);
 
-        //2.3 JELLY_BEAN 一下系统可能用/mnt，这里只支持4.2以上系统
-        //5
-        //mRootFile = new File("/storage");
 
-        //2.4
         mExecutorService =  Executors.newFixedThreadPool(5);
 
     }
 
 
-    //3 建立目录和文件加载的异步任务
+
 
     class DirLoadingTask extends AsyncTask<Object,Void,Object> {
         public DirLoadingTask() {
@@ -133,7 +123,7 @@ public class FileHelper {
                 return  null;
             }
 
-            //3.1
+
             mLock.lock();
 
             try {
@@ -142,7 +132,7 @@ public class FileHelper {
 
             }
             finally {
-                //3.1 此finally块为必须，标准用法
+
                 mLock.unlock();
             }
 
@@ -150,27 +140,27 @@ public class FileHelper {
         }
 
 
-        //6. 硬编码，实现有点不太好
 
-        public static String getRootPath() {
-            return "/storage";
-        }
-
-        public void init() {
-            mCurrentPath = getRootPath();
-        }
-
-        public static boolean isRootDir(String path) {
-            File file = new File(path);
-            return (file.exists() && file.getAbsolutePath().equals("/storage"));
-        }
 
 
 
     }
 
 
+//6. 硬编码，实现有点不太好
 
+    public static String getRootPath() {
+        return "/storage";
+    }
+
+    public void init() {
+        mCurrentPath = getRootPath();
+    }
+
+    public static boolean isRootDir(String path) {
+        File file = new File(path);
+        return (file.exists() && file.getAbsolutePath().equals("/storage"));
+    }
 
 
 
