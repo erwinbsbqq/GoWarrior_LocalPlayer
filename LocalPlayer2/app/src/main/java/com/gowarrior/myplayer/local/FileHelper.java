b297488a6991f2ca8dd4dd515d0e0ac1aef5980f
GoWarrior <support@gowarriorosh.com>
2015-06-05 13:48:57 +0800

    FileHelper,about file method

--- LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/FileHelper.java
+++ LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/FileHelper.java
@@ -6,6 +6,10 @@ import android.os.AsyncTask;
 import com.gowarrior.myplayer.R;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
@@ -37,7 +41,19 @@ public class FileHelper {
 
 
     private String mCurrentPath;
+    private static String mLoadingPath;
+
     private boolean mNeedRefresh = false;
+
+
+    private  DirLoadingTask m_task = null;
+
+    //7 文件信息列表
+    private  ArrayList<FileInfo> mDirEntries = new ArrayList<>();
+    private  ArrayList<FileInfo> mAudioFile  = new ArrayList<>();
+    private  ArrayList<FileInfo> mImageFile  = new ArrayList<>();
+    private  ArrayList<FileInfo> mVideoFile  = new ArrayList<>();
+    private  ArrayList<FileInfo> mApkFile    = new ArrayList<>();
 
     //文件类型
     public enum FILETYPE {
@@ -88,6 +104,9 @@ public class FileHelper {
 
 
 
+
+
+
     private  void initStoragePath() {
 
         mImageSuffixes = mContext.getResources().getStringArray(
@@ -111,7 +130,9 @@ public class FileHelper {
 
 
     class DirLoadingTask extends AsyncTask<Object,Void,Object> {
-        public DirLoadingTask() {
+        private final WeakReference<OnDirLoadedListener> listenerReference;
+        public DirLoadingTask(OnDirLoadedListener listener) {
+            listenerReference = new WeakReference<>(listener);
         }
 
         @Override
@@ -163,14 +184,207 @@ public class FileHelper {
     }
 
 
+    public void loadDir(String path, OnDirLoadedListener listener) {
+
+        if (path == null){
+            return;
+        }
+
+        File file = new File(path);
+        if (file == null ||! file.isDirectory()) {
+            return;
+        }
+
+
+
+
+        //load folder asynchronous
+        cancelLoading();
+        m_task = null;
+
+        loadRootDir(path, listener);
+
+    }
+
+    public void loadRootDir(String path, OnDirLoadedListener listener) {
+        mLoadingPath = path;
+        DirLoadingTask task = new DirLoadingTask(listener);
+        task.executeOnExecutor(mExecutorService, path);
+
+    }
+
+    public void cancelLoading() {
+        if (m_task != null) {
+            m_task.cancel(true);
+        }
+    }
+
+    //7 先建立FileInfo类文件
+
+    public ArrayList<FileInfo>getDirInfo(String path, FILETYPE filetype) {
+        if (path.equals(mCurrentPath)) {
+            ArrayList<FileInfo> infoList = new ArrayList<>();
+            infoList.addAll(mDirEntries);
+
+            switch (filetype) {
+                case APK:
+                    infoList.addAll(mApkFile);
+                    break;
+                case AUDIO:
+                    infoList.addAll(mAudioFile);
+                    break;
+                case IMAGE:
+                    infoList.addAll(mImageFile);
+                    break;
+                case VIDEO:
+                    infoList.addAll(mVideoFile);
+                    break;
+                default:
+                    break;
+            }
+
+            return  infoList;
+        }
+        return  null;
+    }
+
+
+
+
+    public void sortDir(SORTTYPE sortType) {
+        Collections.sort(mDirEntries, new FileInfoNameComparator());
+        Collections.sort(mApkFile, new FileInfoNameComparator());
+        Collections.sort(mAudioFile, new FileInfoNameComparator());
+        Collections.sort(mImageFile, new FileInfoNameComparator());
+        Collections.sort(mVideoFile, new FileInfoNameComparator());
+    }
+
+    public class FileInfoNameComparator implements Comparator<FileInfo> {
+        @Override
+        public int compare(FileInfo file1, FileInfo file2) {
+            return file1.name.compareToIgnoreCase((file2.name));
+        }
+    }
 
 
 
 
 
+    private  boolean loadFiles(String path) {
+        if(path == null ||path.isEmpty()) {
+            return  false;
+        }
+
+        File dir = new File(path);
+        if(!dir.isDirectory()) {
+            return false;
+        }
+
+        File [] fileList = dir.listFiles();
+
+        fillDirInfoList(fileList);
+        sortDir(SORTTYPE.NAME);
 
 
+        return true;
+    }
 
+
+    //fileInfo 数据的填充
+    private void fillDirInfoList(File[] fileList) {
+        if (fileList == null) {
+            return;
+        }
+        boolean isRoot = false;
+        if(fileList.length > 0) {
+            isRoot = isRootDir(fileList[0].getAbsolutePath());
+        }
+
+        for (int i = 0; i < fileList.length; i++) {
+            File file = fileList[i];
+            if (!file.exists()){
+                break;
+            }
+
+            FileInfo fileInfo = new FileInfo();
+            fileInfo.name = file.getName();
+            if (fileInfo.name.startsWith(".")){
+                continue;;
+            }
+            if (isRoot){
+                fileInfo.friendlyName = getStorageFriendlyName(file);
+            } else {
+                fileInfo.friendlyName = fileInfo.name;
+            }
+
+            fileInfo.path  = file.getAbsolutePath();
+            fileInfo.isDir = file.isDirectory();
+
+            fileInfo.filetype = getFileType(fileList[i]);
+
+
+        }
+    }
+
+    //内部方法，用private,显示根目录为“我的U盘-1”之类
+    private String getStorageFriendlyName(File file) {
+        String name = new String("");
+        if (file != null) {
+            name = file.getName().toLowerCase();
+        }
+        if (name.contains("usb")) {
+            name = mFriendlyNameUsb + name.replace("usb", "");
+        }else if (name.contains("sdcard")) {
+            name = mFriendlyNameSd + name;
+        }else if (name.contains("sd")) {
+            name = mFriendlyNameHd + name;
+        }
+
+        return  name;
+    }
+
+    private FILETYPE getFileType(File file) {
+        FILETYPE fileType = FILETYPE.UNKNOW;
+
+        if (file == null) {
+            return  fileType;
+        }
+
+        String name = file.getName();
+
+        if (file.isDirectory()){
+            fileType = FILETYPE.DIR;
+        } else if (name.toLowerCase().endsWith(".apk")) {
+            fileType = FILETYPE.APK;
+            //mAudioSuffixesinitStoragePath等在获取相关值
+        } else if (matchFileType(name, mAudioSuffixes)) {
+            fileType = FILETYPE.AUDIO;
+        } else if (matchFileType(name, mImageSuffixes)) {
+            fileType = FILETYPE.IMAGE;
+        } else if (matchFileType(name, mVideoSuffixes)) {
+            fileType = FILETYPE.VIDEO;
+        }
+
+        return  fileType;
+    }
+
+    //在指定的String数组里找到对应的文件后缀名
+    private boolean matchFileType(String name, String[] suffixes) {
+        boolean match = false;
+        if (name == null || name.isEmpty()) {
+            return  match;
+        }
+        //为了好比较，先全部转成小写
+        String lowerName = name.toLowerCase();
+        for (int i = 0; i < suffixes.length; i++) {
+            if (lowerName.endsWith(suffixes[i])) {
+                match = true;
+                break;
+            }
+        }
+
+        return match;
+    }
 
 
 
---
+++ LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/FileInfo.java
@@ -0,0 +1,20 @@
+package com.gowarrior.myplayer.local;
+
+/**
+ * Created by jerry.xiong on 2015/6/4.
+ */
+public class FileInfo {
+    public String path;
+
+    public String name;
+
+    public String friendlyName;
+
+    public boolean isDir;
+
+    public FileHelper.FILETYPE filetype;
+
+
+
+
+}
