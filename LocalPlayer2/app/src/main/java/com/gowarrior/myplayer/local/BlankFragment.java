e62c58876675b10cf63697b254d4e0f29697385a
GoWarrior <support@gowarriorosh.com>
2015-06-05 14:29:05 +0800

    fragment set default view

--- LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/BlankFragment.java
+++ LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/BlankFragment.java
@@ -1,11 +1,14 @@
 package com.gowarrior.myplayer.local;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.gowarrior.myplayer.R;
 
@@ -16,6 +19,12 @@ import com.gowarrior.myplayer.R;
  */
 public class BlankFragment extends Fragment {
 
+    public final static String LOGTAG = "VideoFragment";
+
+    private GridView gridView;
+    private TextView emptyView;
+    private TextView mCurrentPathView;
+    private TextView mTipView;
 
     private FileHelper mFileHelper;
 
@@ -28,13 +37,22 @@ public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Context context = getActivity();
 
-        mFileHelper = FileHelper.getInstance(getActivity());
+        mFileHelper = FileHelper.getInstance(context);
 
         View rootView;
 
         rootView=inflater.inflate(R.layout.fragment_blank, container, false);
-        //Todo
+
+        gridView  = (GridView)rootView.findViewById(R.id.local_movie_gridView);
+        emptyView = (TextView)rootView.findViewById(R.id.empty_view);
+        mCurrentPathView = (TextView)rootView.findViewById(R.id.current_dir);
+        mTipView  = (TextView)rootView.findViewById(R.id.tip_menu_key);
+
+        gridView.setEmptyView(emptyView);
+
+
 
         return rootView;
     }
