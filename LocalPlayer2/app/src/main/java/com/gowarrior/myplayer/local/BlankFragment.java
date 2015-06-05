efdb95b05bcd56fc743db0099b22e4fa7dd26cfd
GoWarrior <support@gowarriorosh.com>
2015-06-05 15:48:53 +0800

    fragment gridview && implements OnDirLoadedListener

--- LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/BlankFragment.java
+++ LocalPlayer2/app/src/main/java/com/gowarrior/myplayer/local/BlankFragment.java
@@ -4,20 +4,28 @@ package com.gowarrior.myplayer.local;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.gowarrior.myplayer.R;
 
 
+//2
+import com.gowarrior.myplayer.local.FileHelper.OnDirLoadedListener;
+
+import java.io.File;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment extends Fragment {
+                                            //3
+public class BlankFragment extends Fragment implements OnDirLoadedListener {
 
     public final static String LOGTAG = "VideoFragment";
 
@@ -27,6 +35,10 @@ public class BlankFragment extends Fragment {
     private TextView mTipView;
 
     private FileHelper mFileHelper;
+    private ImageWorker mImageWorker;
+
+    private boolean mIsLoading = false;
+    private String mChildName;
 
     public BlankFragment() {
 
@@ -38,10 +50,12 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Context context = getActivity();
+        View rootView;
 
         mFileHelper = FileHelper.getInstance(context);
+        //import ImageWroker类，显示文件和文件夹
+        mImageWorker = new ImageWorker(context);
 
-        View rootView;
 
         rootView=inflater.inflate(R.layout.fragment_blank, container, false);
 
@@ -52,26 +66,92 @@ public class BlankFragment extends Fragment {
 
         gridView.setEmptyView(emptyView);
 
+        //点击时
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //TODO
+
+            }
+        });
+        //选中时  ,OnItemSelectedListener api 19和22有区别
+        gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //当前目录下文件数
+                    updatePageNumber();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        gridView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                updatePageNumber();
+
+            }
+        });
+
+        gridView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                String currentPath = mFileHelper.getCurrentPath();
+                if (currentPath == null|| currentPath.isEmpty()) {
+                    return  false;
+                }
+
+                File currentFile = new File(mFileHelper.getCurrentPath());
+                switch (event.getKeyCode()){
+                    case KeyEvent.KEYCODE_BACK:
+                        if (FileHelper.isRootDir(mFileHelper.getCurrentPath())) {
+                            mFileHelper.cancelLoading();
+                            mIsLoading = false;
+                            return false;
+                        }
+                        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                            mIsLoading =true;
+                            mTipView.setText("Loading");
+
+                            //when back to /storage/ext, back to /storage direct for pair with enter action
+                            //b暂时挪过来的一段代码
+                            if (currentFile.getParentFile().getAbsolutePath().equals("/storage/ext")) {
+                                mFileHelper.setCurrentPath("/storage/ext");
+                                currentFile = new File(mFileHelper.getCurrentPath());
+                            }
+                            //e暂时挪过来的一段代码
+
+                            mChildName = currentFile.getName();
+                            //1.直接输入BlankFragment.this报错，要引入接口OnDirLoadedListener
+                            mFileHelper.loadDir(currentFile.getParentFile().getAbsolutePath(),BlankFragment.this);
+
+                        }
+                }
+
+                return false;
+            }
+        });
+
+
+
+
 
 
         return rootView;
     }
 
+    private void updatePageNumber() {
+        //TODO
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public void onDirLoaded(String Path) {
+        //TODO
+    }
 }
